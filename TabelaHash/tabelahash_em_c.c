2ada9116a67141e7f06cfcc684ef4064b7083f0f
Mickeias Charles <charlesmickeias@gmail.com>
2025-05-28 16:28:56 -0300

    Update tabelahash_em_c.c

--- TabelaHash/tabelahash_em_c.c
+++ TabelaHash/tabelahash_em_c.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-// Definindo a struct Item
+// Definindo a struct
 typedef struct {
     char* chave;
     int valor;
 } Item;
 
-// Função de busca linear
+// Função de uma busca linear
 Item* busca_linear(Item* itens, size_t tamanho, const char* chave) {
     for (size_t i = 0; i < tamanho; i++) {
         if (strcmp(itens[i].chave, chave) == 0) {
@@ -20,21 +20,21 @@ Item* busca_linear(Item* itens, size_t tamanho, const char* chave) {
 int main(void) {
     // Vetor de itens
     Item itens[] = {
-        {"foo", 10}, {"bar", 42}, {"bazz", 36}, {"buzz", 7},
-        {"bob", 11}, {"jane", 100}, {"x", 200}
+        {"mickeias", 13}, {"weldes", 42}, {"onibus", 36}, {"brasil", 7},
+        {"tallys", 11}, {"antonio", 100}, {"metro", 200}
     };
 
     size_t quantidade_itens = sizeof(itens) / sizeof(Item);
 
     // Procurando pelo item "bob"
-    Item* encontrado = busca_linear(itens, quantidade_itens, "bob");
+    Item* encontrado = busca_linear(itens, quantidade_itens, "mickeias");
 
     if (!encontrado) {
         printf("Item não encontrado!\n");
         return 1;
     }
 
-    printf("Valor de 'bob' é %d\n", encontrado->valor);
+    printf("Valor de 'mickeias' é %d\n", encontrado->valor);
 
     return 0;
 }
